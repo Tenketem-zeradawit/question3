2f79c6c24e8d133e9e2bde98a3d9a10e22509983
Tenketem-zeradawit <titihadas123@gmail.com>
2024-09-29 12:58:26 -0700

    Add files via upload

---
+++ lab3.js
@@ -0,0 +1,14 @@
+
+function addJavaScript(str) {
+    return "JavaScript " + str;
+}
+
+let sumString = "is the language that makes the web interactive !";
+
+// Call the function and log the result
+let result = addJavaScript(sumString);
+console.log(result);
+
+
+
+
